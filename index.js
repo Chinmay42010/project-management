74ca04059fda815c4ae2879a354f4efa6b01447b
chinmay <2vd23ci401@klsvdit.edu.in>
2026-08-05 13:30:23 +0530

    added env

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`Start of backend project.`);
+import dotenv from "dotenv"
+
+process.env.username
+
+dotenv.config({
+    path:"./.env",
+})
+
+const myusername = process.env.username;
+
+
+console.log(`Start of backend project`);
+console.log(myusername);
